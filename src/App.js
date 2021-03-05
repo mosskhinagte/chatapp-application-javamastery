4781ab2b116ca165860439ee35fa16a2d5a419b9
mosskhinagte <moseschorei11@gmail.comm>
2021-03-05 06:34:42 -0800

    chatapp-application-javamastery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const App = () => {
+        if(!localStorage.getItem('username')) return <LoginForm/>
+    
+
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="f520c0ad-52ae-4ad7-8d14-2932319afb72"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed{...chatAppProps} />}
+        />
+
+    )
 }
 
 export default App;
